42c6aa302e9bb88fab9448af36890e36154516bb
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-01-16 16:02:38 +0100

    first commit

---
+++ app/lib/definitions.ts
@@ -0,0 +1,88 @@
+// Este archivo contiene definiciones de tipos para tus datos.
+// Describe la forma de los datos, y qué tipo de datos debe aceptar cada propiedad.
+// Para simplificar la enseñanza, estamos definiendo manualmente estos tipos.
+// Sin embargo, estos tipos se generan automáticamente si estás usando un ORM como Pris
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+};
+
+export type Invoice = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  date: string;
+  // En TypeScript, esto se llama un tipo de unión de cadena.
+  // Significa que la propiedad "status" solo puede ser una de las dos cadenas: 'pending' o 'paid'.
+  status: 'pending' | 'paid';
+};
+
+export type Revenue = {
+  month: string;
+  revenue: number;
+};
+
+export type LatestInvoice = {
+  id: string;
+  name: string;
+  image_url: string;
+  email: string;
+  amount: string;
+};
+
+// La base de datos devuelve un número para el importe, pero después lo formateamos a una cadena con la función formatCurrency
+export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+  amount: number;
+};
+
+export type InvoicesTable = {
+  id: string;
+  customer_id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  date: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
+
+export type CustomersTableType = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: number;
+  total_paid: number;
+};
+
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
+};
+
+export type CustomerField = {
+  id: string;
+  name: string;
+};
+
+export type InvoiceForm = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Lemon } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+});
+export const lemon = Lemon({ 
+    weight: ['400'],
+    subsets: ['latin'] 
+});
